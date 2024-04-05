f3575977c5dd2c51d355bfff188fa5fed250bf7b
giorgi568 <mateshvili02@mail.ru>
2024-04-06 02:41:02 +0400

    added discover component

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,77 @@
+import { useState } from 'react';
 import './App.css';
 import Discover from './components/Discover';
 
 function App() {
+  const [navScroll, setNavScroll] = useState(false);
+  const changeScroll = () => {
+    if (window.scrollY > 1) {
+      setNavScroll(true);
+    } else {
+      setNavScroll(false);
+    }
+  };
+  window.addEventListener('scroll', changeScroll);
   return (
-    <div className='content'>
-      <nav>
-        <h2 className='logoText'>ExploreEra</h2>
-        <div className='navWrapper'>
-          <a href='http://' target='_blank' rel='noopener noreferrer'>
-            Home
-          </a>
-          <a href='http://' target='_blank' rel='noopener noreferrer'>
-            About us
-          </a>
-          <a href='http://' target='_blank' rel='noopener noreferrer'>
-            Blogs
-          </a>
-          <a href='http://' target='_blank' rel='noopener noreferrer'>
-            Our services
-          </a>
-          <a href='http://' target='_blank' rel='noopener noreferrer'>
-            Our offers
-          </a>
-          <svg
-            width='40'
-            height='41'
-            viewBox='0 0 40 41'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-            className='userIcon'
+    <div>
+      <div className='content'>
+        <header className={navScroll && 'scrolling'}>
+          <h2
+            className='logoText'
+            style={navScroll ? { color: '#424244' } : {}}
           >
-            <path
-              d='M31.6673 35.5C31.6673 29.0567 26.444 23.8333 20.0007 23.8333C13.5573 23.8333 8.33398 29.0567 8.33398 35.5M20.0007 18.8333C16.3188 18.8333 13.334 15.8486 13.334 12.1667C13.334 8.48477 16.3188 5.5 20.0007 5.5C23.6825 5.5 26.6673 8.48477 26.6673 12.1667C26.6673 15.8486 23.6825 18.8333 20.0007 18.8333Z'
-              stroke='#424244'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-          </svg>
-        </div>
-      </nav>
-      <div className="visitContainer">
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-          <h3>Visit Mountains In</h3>
-          <h1>ITALY</h1>
-          <button className='seeOfferBtn'>See offer</button>
+            ExploreEra
+          </h2>
+          <div
+            className='navWrapper'
+            style={navScroll ? { color: '#424244' } : { color: 'white' }}
+          >
+            <a href='http://' target='_blank' rel='noopener noreferrer'>
+              Home
+            </a>
+            <a href='http://' target='_blank' rel='noopener noreferrer'>
+              About us
+            </a>
+            <a href='http://' target='_blank' rel='noopener noreferrer'>
+              Blogs
+            </a>
+            <a href='http://' target='_blank' rel='noopener noreferrer'>
+              Our services
+            </a>
+            <a href='http://' target='_blank' rel='noopener noreferrer'>
+              Our offers
+            </a>
+            <svg
+              width='32'
+              height='35'
+              viewBox='0 0 40 41'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+              className='userIcon'
+            >
+              <path
+                d='M31.6673 35.5C31.6673 29.0567 26.444 23.8333 20.0007 23.8333C13.5573 23.8333 8.33398 29.0567 8.33398 35.5M20.0007 18.8333C16.3188 18.8333 13.334 15.8486 13.334 12.1667C13.334 8.48477 16.3188 5.5 20.0007 5.5C23.6825 5.5 26.6673 8.48477 26.6673 12.1667C26.6673 15.8486 23.6825 18.8333 20.0007 18.8333Z'
+                stroke={navScroll ? '#424244' : 'white'}
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              />
+            </svg>
+          </div>
+        </header>
+        <div className='visitContainer'>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <h3>Visit Mountains In</h3>
+            <h1>ITALY</h1>
+            <button className='seeOfferBtn'>See offer</button>
+          </div>
         </div>
       </div>
       <Discover></Discover>
--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,15 +1,65 @@
+import styles from '../styles/discover.module.css';
+
 function Discover() {
   return (
-    <div>
-      <h3>Discover Our Services</h3>
-      <p>
+    <div className={styles.content}>
+      <h3 className={styles.mainHeader}>Discover Our Services</h3>
+      <p className={styles.mainP}>
         ExploreEra provides a one-stop solution for individuals seeking
         well-planned journeys. These services include expert advice on
         destination selection, flight and accommodation bookings, and customized
         itineraries to individual preferences.
       </p>
-      <div className='card'>
-        <img src='assets/react.svg' alt='photo of a plane' />
+      <a className={styles.seeall} href="http://" target="_blank" rel="noopener noreferrer">see all</a>
+      <div className={styles.cardWrapper}>
+        <div className={styles.card}>
+          <img
+            src='servicesImgs/plane.png'
+            alt='photo of a plane'
+            className={styles.img}
+          />
+          <h4 className={styles.cardHeader}>Book Flight</h4>
+          <p className={styles.cardP}>
+            book memorable journey with our flight booking services! we&apos;re
+            thrilled to present you flight options to suit your travel needs.
+          </p>
+        </div>
+        <div className={styles.card}>
+          <img
+            src='servicesImgs/pool.png'
+            alt='photo of a plane'
+            className={styles.img}
+          />
+          <h4 className={styles.cardHeader}>Book Hotels</h4>
+          <p className={styles.cardP}>
+            book memorable journey with our flight booking services! we&apos;re
+            thrilled to present you flight options to suit your travel needs.
+          </p>
+        </div>
+        <div className={styles.card}>
+          <img
+            src='servicesImgs/cruise.png'
+            alt='photo of a plane'
+            className={styles.img}
+          />
+          <h4 className={styles.cardHeader}>Book Cruises</h4>
+          <p className={styles.cardP}>
+            book memorable journey with our flight booking services! we&apos;re
+            thrilled to present you flight options to suit your travel needs.
+          </p>
+        </div>
+        <div className={styles.card}>
+          <img
+            src='servicesImgs/boat.png'
+            alt='photo of a plane'
+            className={styles.img}
+          />
+          <h4 className={styles.cardHeader}>Book family Vacation</h4>
+          <p className={styles.cardP}>
+            book memorable journey with our flight booking services! we&apos;re
+            thrilled to present you flight options to suit your travel needs.
+          </p>
+        </div>
       </div>
     </div>
   );
