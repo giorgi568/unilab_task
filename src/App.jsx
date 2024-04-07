ed11e3c17ecc59b12009c50a58030ff8af447f8a
giorgi568 <mateshvili02@mail.ru>
2024-04-07 19:39:05 +0400

    added tooltip for signup

--- src/App.jsx
+++ src/App.jsx
@@ -46,22 +46,32 @@ function App() {
             <a href='http://' target='_blank' rel='noopener noreferrer'>
               Our offers
             </a>
-            <svg
-              width='32'
-              height='35'
-              viewBox='0 0 40 41'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-              className='userIcon'
-            >
-              <path
-                d='M31.6673 35.5C31.6673 29.0567 26.444 23.8333 20.0007 23.8333C13.5573 23.8333 8.33398 29.0567 8.33398 35.5M20.0007 18.8333C16.3188 18.8333 13.334 15.8486 13.334 12.1667C13.334 8.48477 16.3188 5.5 20.0007 5.5C23.6825 5.5 26.6673 8.48477 26.6673 12.1667C26.6673 15.8486 23.6825 18.8333 20.0007 18.8333Z'
-                stroke={navScroll ? '#424244' : 'white'}
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              />
-            </svg>
+            <div className='iconContainer'>
+              <svg
+                width='32'
+                height='35'
+                viewBox='0 0 40 41'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+                className='userIcon'
+              >
+                <path
+                  d='M31.6673 35.5C31.6673 29.0567 26.444 23.8333 20.0007 23.8333C13.5573 23.8333 8.33398 29.0567 8.33398 35.5M20.0007 18.8333C16.3188 18.8333 13.334 15.8486 13.334 12.1667C13.334 8.48477 16.3188 5.5 20.0007 5.5C23.6825 5.5 26.6673 8.48477 26.6673 12.1667C26.6673 15.8486 23.6825 18.8333 20.0007 18.8333Z'
+                  stroke={navScroll ? '#424244' : 'white'}
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+              <div className='tooltip'>
+                <a href='#0'>Sign In</a>
+                <span className='line'></span>
+                <a href='#0'>Sign Up</a>
+                <span className='line'></span>
+                <a href='#0'>Sign Out</a>
+                <span className='line'></span>
+              </div>
+            </div>
           </div>
         </header>
         <div className='visitContainer'>
